99b08801648dcd15698c4aea506803e169bc5054
jooler <jerr@foxmail.com>
2024-08-18 19:57:04 +0800

    使用graphql中间件对graphql操作进行鉴权

--- src/index.js
+++ src/index.js
@@ -122,7 +122,59 @@ module.exports = {
             },
           ],
         },
-        'Mutation.updateUsersBasicinfo': {
+        'Mutation.updateMessage': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let message = await strapi.entityService.findOne('api::message.message',args.id, {
+                      populate: {
+                          sender: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(message?.sender?.id !== id){
+                      throw new Error('you have no auth to edit this message');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.deleteMessage': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let message = await strapi.entityService.findOne('api::message.message',args.id, {
+                      populate: {
+                          sender: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(message?.sender?.id !== id){
+                      throw new Error('you have no auth to edit this message');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.updateUsersPermissionsUser': {
           middlewares: [
             async (next, parent, args, context, info) => {
                 // console.log('context', context)
@@ -133,7 +185,7 @@ module.exports = {
                   let user = await strapi.entityService.findOne('plugin::users-permissions.user',id);
                 //   console.log('user', user)
                   if(!user){
-                      throw new Error('channel not exists');
+                      throw new Error('user not exists');
                   } else {
                       args.id = user.id;
                     //   console.log('args', args)
@@ -170,6 +222,298 @@ module.exports = {
                           args.data.author = id;
                       }
                       console.log('args', args)
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.updateElement': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+            //   console.log('args', args)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let element = await strapi.entityService.findOne('api::element.element',args.id,{
+                      populate: {
+                          creator: {
+                              fields: ['id']
+                          },
+                          author: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(!element?.creator?.id !== id && !element?.author?.id !== id){
+                      throw new Error('this element not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.deleteElement': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+            //   console.log('args', args)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let element = await strapi.entityService.findOne('api::element.element',args.id,{
+                      populate: {
+                          creator: {
+                              fields: ['id']
+                          },
+                          author: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(!element?.creator?.id !== id && !element?.author?.id !== id){
+                      throw new Error('this element not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Query.favorite': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+            //   console.log('args', args)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let user = await strapi.entityService.findOne('plugin::users-permissions.user',id,{
+                      populate: {
+                          favorites: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(!user?.favorites.map(i => i.id)?.includes(args.id)){
+                      throw new Error('this favorite not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Query.favorites': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+            //   console.log('args', args)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  if(!args.filters){
+                      args.filters = {}
+                  }
+                  args.filters.owner = {
+                      id: {
+                        eq: id
+                       }
+                  }
+                  const res = await next(parent, args, context, info);
+                  return res
+              }
+            },
+          ],
+        },
+        'Mutation.updateFavorite': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let favorite = await strapi.entityService.findOne('api::favorite.favorite',args.id, {
+                      populate: {
+                          owner: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(favorite?.owner?.id !== id){
+                      throw new Error('this favorite is not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.deleteFavorite': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let favorite = await strapi.entityService.findOne('api::favorite.favorite',args.id, {
+                      populate: {
+                          owner: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(favorite?.owner?.id !== id){
+                      throw new Error('this favorite is not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.createBizcard': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  args.data.user = id
+                  const res = await next(parent, args, context, info);
+                  return res
+              }
+            },
+          ],
+        },
+        'Mutation.updateBizcard': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let bizcard = await strapi.entityService.findOne('api::bizcard.bizcard',args.id, {
+                      populate: {
+                          user: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(bizcard?.user?.id !== id){
+                      throw new Error('this bizcard is not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.deleteBizcard': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let bizcard = await strapi.entityService.findOne('api::bizcard.bizcard',args.id, {
+                      populate: {
+                          user: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(bizcard?.user?.id !== id){
+                      throw new Error('this bizcard is not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.createBizcardCollection': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  args.data.user = id
+                  const res = await next(parent, args, context, info);
+                  return res
+              }
+            },
+          ],
+        },
+        'Mutation.updateBizcardCollection': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let bizcard = await strapi.entityService.findOne('api::bizcard-collection.bizcard-collection',args.id, {
+                      populate: {
+                          user: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(bizcard?.user?.id !== id){
+                      throw new Error('this bizcard collection is not belong to you or not exists');
+                  } else {
+                      const res = await next(parent, args, context, info);
+                      return res
+                  }
+              }
+            },
+          ],
+        },
+        'Mutation.deleteBizcardCollection': {
+          middlewares: [
+            async (next, parent, args, context, info) => {
+                // console.log('context', context)
+              const { id } = context.state.user;
+              if(!id){
+                  throw new Error('User is not authenticated');
+              } else {
+                  let bizcard = await strapi.entityService.findOne('api::bizcard.bizcard',args.id, {
+                      populate: {
+                          user: {
+                              fields: ['id']
+                          }
+                      }
+                  });
+                //   console.log('user', user)
+                  if(bizcard?.user?.id !== id){
+                      throw new Error('this bizcard collection is not belong to you or not exists');
+                  } else {
                       const res = await next(parent, args, context, info);
                       return res
                   }
